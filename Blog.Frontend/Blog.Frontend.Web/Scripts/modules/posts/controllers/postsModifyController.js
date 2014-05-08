ad222fddf2922e29dad4fa6bfeec710ad8ca3baf
Jason Magpantay <jsnmgpnty@gmail.com>
2014-05-09 06:48:42 +0800

    Added support for file uploads

--- Blog.Frontend/Blog.Frontend.Web/Scripts/modules/posts/controllers/postsModifyController.js
+++ Blog.Frontend/Blog.Frontend.Web/Scripts/modules/posts/controllers/postsModifyController.js
@@ -1,6 +1,22 @@
-﻿ngPosts.controller('postsModifyController', ["$scope", "$interval", "snapRemote", "localStorageService", "postsService", "postsStateService", "blockUiService", "dateHelper",
-    function ($scope, $interval, snapRemote, localStorageService, postsService, postsStateService, blockUiService, dateHelper) {
+﻿ngPosts.controller('postsModifyController', ["$scope", "$interval", "$fileUploader", "snapRemote", 
+    "localStorageService", "postsService", "postsStateService", "blockUiService", "dateHelper", "configProvider",
+    function ($scope, $interval, $fileUploader, snapRemote, localStorageService, postsService,
+        postsStateService, blockUiService, dateHelper, configProvider) {
         $scope.post = null;
-        
+        $scope.username = localStorageService.get("username");
+        $scope.uploadUrl = configProvider.getSettings().BlogApi == "" ?
+            window.blogConfiguration.blogApi + "upload?username=" + $scope.username :
+            configProvider.getSettings().BlogApi + "upload?username=" + $scope.username;
+
+        var uploader = $scope.uploader = $fileUploader.create({
+            scope: $scope,
+            url: $scope.uploadUrl
+        });
+
+        uploader.filters.push(function (item /*{File|HTMLInputElement}*/) {
+            var type = uploader.isHTML5 ? item.type : '/' + item.value.slice(item.value.lastIndexOf('.') + 1);
+            type = '|' + type.toLowerCase().slice(type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        });
     }
 ]);
---
+++ Blog.Frontend/Blog.Frontend.Web/Scripts/modules/posts/directives/postContentUploadItem.js
@@ -0,0 +1,10 @@
+﻿ngPosts.directive("postContentUploadItem", [
+    function() {
+        return {
+            restrict: 'EA',
+            scope: { item: '=' },
+            replace: true,
+            templateUrl: window.blogConfiguration.templatesUrl + "posts/postContentUploadItem.html"
+        };
+    }
+]);
