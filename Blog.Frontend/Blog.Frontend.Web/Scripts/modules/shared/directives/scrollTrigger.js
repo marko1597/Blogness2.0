c4390bbf807b7749f34607924389d3f6a10856c2
jsnmgpnty <jsnmgpnty@gmail.com>
2014-05-15 20:46:49 +0800

    - Added missing files

---
+++ Blog.Frontend/Blog.Frontend.Web/Scripts/modules/shared/directives/scrollTrigger.js
@@ -0,0 +1,15 @@
+﻿ngShared.directive('scrollTrigger', ["$rootScope", function ($rootScope) {
+    return {
+        link: function (scope, element, attrs) {
+            var elemWatch = "#" + attrs.scrollTriggerWatch;
+            var threshold = attrs.scrollTriggerThreshold;
+
+            angular.element(element).bind("scroll", function() {
+                var scroll = $(element).scrollTop();
+                if (scroll >= $(elemWatch).outerHeight() - threshold) {
+                    $rootScope.$broadcast("scrollBottom");
+                }
+            });
+        }
+    };
+}]);
