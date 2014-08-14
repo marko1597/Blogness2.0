dab41474f84fb61fc53e561bdc4cfe5c22b8bb01
jsnmgpnty <jsnmgpnty@gmail.com>
2014-08-14 23:07:11 +0800

    Added missing userProfileNavigation file

---
+++ Blog.Web/Blog.Web.Site/Scripts/modules/user/directives/userProfileNavigation.js
@@ -0,0 +1,27 @@
+﻿ngUser.directive('userProfileNavigation', [function () {
+    var ctrlFn = function ($scope) {
+        $scope.aboutUrl = ($scope.username == null || $scope.username === "undefined") ?
+            "/#/user" : "#/user/" + $scope.username;
+
+        $scope.postsUrl = ($scope.username == null || $scope.username === "undefined") ?
+            "/#/user/posts" : "#/user/" + $scope.username + "/posts";
+
+        $scope.commentsUrl = ($scope.username == null || $scope.username === "undefined") ?
+            "/#/user/comments" : "#/user/" + $scope.username + "/comments";
+
+        $scope.mediaUrl = ($scope.username == null || $scope.username === "undefined") ?
+            "/#/user/media" : "#/user/" + $scope.username + "/media";
+
+        $scope.favoritesUrl = ($scope.username == null || $scope.username === "undefined") ?
+            "/#/user/favorites" : "#/user/" + $scope.username + "/favorites";
+    };
+    ctrlFn.$inject = ["$scope"];
+
+    return {
+        restrict: 'EA',
+        scope: { username: '=' },
+        replace: true,
+        templateUrl: window.blogConfiguration.templatesModulesUrl + "user/userProfileNavigation.html",
+        controller: ctrlFn
+    };
+}]);
