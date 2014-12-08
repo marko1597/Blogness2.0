2b294c7810dcee7d4fdf117ea66897c29666b2c7
Jason Magpantay <jsnmgpnty@gmail.com>
2014-12-08 09:14:35 +0800

    Somewhat of a fix for duplicate row entries for community

--- Blog.DataAccess/Blog.DataAccess.Database/Repository/UserRepository.cs
+++ Blog.DataAccess/Blog.DataAccess.Database/Repository/UserRepository.cs
@@ -22,58 +22,60 @@ namespace Blog.DataAccess.Database.Repository
 
         public override User Edit(User user)
         {
-            var db = Find(a => a.UserId == user.UserId, null, null).FirstOrDefault();
-            if (db == null) throw new Exception("Record not found!");
-
-            #region Joined Communities
-
-            //var tempJoinedCommunityNames = db.JoinedCommunities.Select(a => a.Name).ToList();
-            //var tempJoinedCommunities = tempJoinedCommunityNames.Select(c => 
-            //    Context.Communities.FirstOrDefault(a => a.Name == c)).ToList();
-
-            var tempJoinedCommunities = db.JoinedCommunities.ToList();
-            foreach (var c in tempJoinedCommunities)
+            using (var context = new BlogDb())
             {
-                Context.Entry(c).State = GetCommunityState(c, user.JoinedCommunities);
+                context.Configuration.ProxyCreationEnabled = false;
+
+                var db = context.Users
+                    .Include(a => a.CreatedCommunities)
+                    .Include(a => a.JoinedCommunities)
+                    .FirstOrDefault(a => a.UserId == user.UserId);
+
+                if (db == null) throw new Exception("Record not found!");
+
+                #region Joined Communities
+
+                var tempJoinedCommunities = db.JoinedCommunities.ToList();
+                foreach (var c in tempJoinedCommunities)
+                {
+                    context.Entry(c).State = GetCommunityState(c, user.JoinedCommunities);
+                }
+
+                var newCommunities = RepositoryHelper.GetNewCommunities(db.JoinedCommunities, user.JoinedCommunities, user.UserId);
+                newCommunities.ForEach(a =>
+                {
+                    context.Communities.Attach(a);
+                    context.Entry(a).State = EntityState.Added;
+                    db.JoinedCommunities.Add(a);
+                });
+
+                #endregion
+
+                #region Created Communities
+
+                var tempCreatedCommunities = db.CreatedCommunities.ToList();
+
+                foreach (var c in tempCreatedCommunities)
+                {
+                    context.Entry(c).State = EntityState.Unchanged;
+                }
+
+                #endregion
+
+                db.FirstName = user.FirstName;
+                db.LastName = user.LastName;
+                db.EmailAddress = user.EmailAddress;
+                db.BirthDate = user.BirthDate;
+                db.UserName = user.UserName;
+                db.BackgroundId = user.BackgroundId;
+                db.PictureId = user.PictureId;
+                db.IdentityId = user.IdentityId;
+
+                context.Entry(db).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return db; 
             }
-
-            var newCommunities = RepositoryHelper.GetNewCommunities(db.JoinedCommunities, user.JoinedCommunities, user.UserId);
-            newCommunities.ForEach(a =>
-            {
-                Context.Communities.Attach(a);
-                Context.Entry(a).State = EntityState.Added;
-                db.JoinedCommunities.Add(a);
-            });
-
-            #endregion
-
-            #region Created Communities
-
-            //var tempCreatedCommunities = Context.Communities.Distinct()
-            //    .Where(a => a.LeaderUserId == user.UserId).ToList();
-
-            //var tempCreatedCommunities = db.CreatedCommunities.ToList();
-
-            //foreach (var c in tempCreatedCommunities)
-            //{
-            //    Context.Entry(c).State = EntityState.Unchanged;
-            //}
-            
-            #endregion
-
-            db.FirstName = user.FirstName;
-            db.LastName = user.LastName;
-            db.EmailAddress = user.EmailAddress;
-            db.BirthDate = user.BirthDate;
-            db.UserName = user.UserName;
-            db.BackgroundId = user.BackgroundId;
-            db.PictureId = user.PictureId;
-            db.IdentityId = user.IdentityId;
-
-            Context.Entry(db).State = EntityState.Modified;
-            Context.SaveChanges();
-
-            return db;
         }
 
         private static EntityState GetCommunityState(Community community, IEnumerable<Community> communities)
